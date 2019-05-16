4c56bcbb3a81f639eb592c26577be4ac95c13441
Sam Bensley <sam.bensley@me.com>
2019-05-16 15:20:09 +1000

    little changes

--- main.py
+++ main.py
@@ -16,50 +16,30 @@ epw = pdf.w - 2*pdf.l_margin
  
 # Set column width to 1/4 of effective page width to distribute content 
 # evenly across table and page
-col_width = epw/4
+col_width = epw/2
  
 # Since we do not need to draw lines anymore, there is no need to separate
 # headers from data matrix.
  
-data = [['First name im an going to put wang to happpen','Last name','Age','City'],
-['Jules','Smith',34,'San Juan'],
-['Mary','Ramos',45,'Orlando'],[
-'Carlson','Banks',19,'Los Angeles']
-]
+data = [['First name im an going to put wang to happpen','Last name'],
+['Jules','Smith'],
+['Mary','Ramos'],[
+'Carlson','Banks']]
  
 # Document title centered, 'B'old, 14 pt
-pdf.set_font('Times','B',14.0) 
-pdf.cell(epw, 0.0, 'Demographic data', align='C')
 pdf.set_font('Times','',10.0) 
 pdf.ln(0.5)
  
 # Text height is the same as current font size
 th = pdf.font_size
  
-for row in data:
-    for datum in row:
-        # Enter data in colums
-        # Notice the use of the function str to coerce any input to the 
-        # string type. This is needed
-        # since pyFPDF expects a string, not a number.
-        pdf.cell(col_width, th, str(datum), border=1)
- 
-    pdf.ln(th)
- 
-# Line break equivalent to 4 lines
-pdf.ln(4*th)
- 
-pdf.set_font('Times','B',14.0) 
-pdf.cell(epw, 0.0, 'With more padding', align='C')
-pdf.set_font('Times','',10.0) 
-pdf.ln(0.5)
- 
 # Here we add more padding by passing 2*th as height
 for row in data:
+    
     for datum in row:
         # Enter data in colums
-        pdf.cell(col_width, 2*th, str(datum), border=1)
- 
-    pdf.ln(2*th)
+        pdf.multicell(col_width, 5*th, str(datum), border=0)
+        
+    pdf.ln(5*th)
  
 pdf.output('table-using-cell-borders.pdf','F')
